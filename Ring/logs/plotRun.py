f4362a397d2d7eb6c81473fe1620a3b6188faa16
VladislavTempez <vladislav.tempez@ens-rennes.fr>
2016-05-23 16:44:32 +0200

    new plotting script

--- Ring/logs/plotRun.py
+++ Ring/logs/plotRun.py
@@ -72,5 +72,5 @@ for f in range(popSize-learnersNumber):
     plt.plot(posHistoryA[f][ runDuration - maxPlot : runDuration - 1],color='g')
 for f in range(learnersNumber):
     plt.plot(posHistoryL[f][runDuration - maxPlot : runDuration - 1],'ro',color='r')
-plt.title('position of fishes')
+plt.title('position of fishes (learners in red)')
 plt.show()
