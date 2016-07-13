7e53631a45535b3b622393c05cde9f266a0de437
VladislavTempez <vladislav.tempez@ens-rennes.fr>
2016-07-13 16:09:26 +0200

    new metrics display scripts

---
+++ Ring/logs/firstTimeInGroupPlotFunction.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from curves import *
+def plotFirstTimeInGroup(infos,firstTimeInGroup):
+ 
+    [
+            runDuration,
+            popSize,
+            ringSize,
+            decreasePoint,
+            decreaseValue,
+            learnersNumber,
+            adultsNumber,
+            reward,
+            penalty,
+            minSizeOfGroup,
+            minimumDistanceToBeInGroup,
+            pointToStopExploration,
+            cycleLength
+            ] = infos
+
+    plt.plot(smoothCurve(firstTimeInGroup))
+    plt.xlabel('Time in fractions of learning phase')
+    plt.ylabel('Time for the first group to be created')
+    plt.title('Time for group creation during the learning phase')
+    plt.show()
+    return
--- Ring/logs/plotRun.py
+++ Ring/logs/plotRun.py
@@ -1,12 +1,8 @@
 from loadLogs import *
-from joinGroupDatePlotFunction import *
 from positionPlotFunction import *
-from timeInGroupPlotFunction import *
-from timeOutsideGroupPlotFunction import *
-from averageNumberOfRewardPlotFunction import *
-from groupnessPlotFunction import *
-from groupExistencePlotFunction import *
 from computableMetrics import *
+from firstTimeInGroupPlotFunction import *
+from neighbourNumberPlotFunction import *
 from sys import argv
 if len(argv) > 1:
     script, filename = argv
@@ -16,17 +12,13 @@ else:
 logsLocFile=open(filename)
 logsFileName=logsLocFile.read().splitlines()[0]
 logsLocFile.close()
-infos,timeOfReward,joinGroupDateLearnersHist,timeInGroupLearnersHist,joinGroupDateAdultsHist,timeInGroupAdultsHist,posHistoryA,posHistoryL = load(logsFileName)
-plotMetrics(infos,posHistoryA,posHistoryL)
 
-#plotJoinGroup(infos,joinGroupDateAdultsHist,joinGroupDateLearnersHist)
+infos,sumSquaredDistanceHist,firstTimeInGroupHist,maxNumberOfNeighbour,minNumberOfNeighbour,posHistoryA,posHistoryL = load(logsFileName)
+
+plotFirstTimeInGroup(infos,firstTimeInGroupHist)
+
+plotNumberOfNeighbour(infos,minNumberOfNeighbour,maxNumberOfNeighbour)
+
+#plotMetrics(infos,posHistoryA,posHistoryL)
 
 plotPos(infos,posHistoryA,posHistoryL)
-
-#plotTimeInGroup(infos,timeInGroupLearnersHist,timeInGroupAdultsHist)
-
-#plotTimeOutGroup(infos,timeInGroupLearnersHist,timeInGroupAdultsHist,joinGroupDateLearnersHist,joinGroupDateAdultsHist)
-
-learnersNumber = infos[2]
-#if learnersNumber > 0:
-#    plotAverageReward(infos,timeOfReward)
